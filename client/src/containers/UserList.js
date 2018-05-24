4cc9a8d9e90cd152a9d4b927bef84cc13b895775
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-05-24 12:36:03 +0100

    Add custom scrollbar

--- client/src/containers/UserList.js
+++ client/src/containers/UserList.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import Username from 'components/Username';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import styles from './UserList.module.css';
 
 export default class UserList extends Component {
   constructor(props) {
     super(props);
+
+    this.renderThumb = ({ style, ...props }) => {
+      const thumbStyle = {
+        backgroundColor: 'rgba(144, 164, 173, 0.5)',
+      };
+      return <div style={{ ...style, ...thumbStyle }} {...props} />;
+    };
   }
   render() {
     return (
       <div className={styles.listContainer}>
-        <Username name="Tom" />
-        <Username name="Tom" />
-        <Username name="Tom" />
-        <Username name="Tom" />
-        <Username name="Tom" />
+        <Scrollbars renderThumbVertical={this.renderThumb}>
+          <Username name="Tom" />
+          <Username name="Tom" />
+          <Username name="Tom" />
+          <Username name="Tom" />
+          <Username name="Tom" />
+          <Username name="Tom" />
+          <Username name="Tom" />
+          <Username name="Tom" />
+          <Username name="Tom" />
+          <Username name="Tom" />
+          <Username name="Tom" />
+          <Username name="Tom" />
+          <Username name="Tom" />
+        </Scrollbars>
       </div>
     );
   }

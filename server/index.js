17655869c82f2bb6485c8c0a64e6ea935732412d
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-05-24 21:10:23 +0100

    Setup basic socket server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
-const app = require('express')();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
 const port = process.env.PORT || 3001;
+const index = require('./routes/index');
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+const io = socketIo(server);
 
 io.on('connection', socket => {
   console.log('a user connected');
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('disconnected', () => {
+    console.log('disconnected');
   });
 
   socket.on('chat', data => {
@@ -16,6 +23,9 @@ io.on('connection', socket => {
   });
 });
 
-server.listen(port, () => {
+server.listen(port, err => {
+  if (err) {
+    throw new Error(err);
+  }
   console.log(`listening on *:${port}`);
 });

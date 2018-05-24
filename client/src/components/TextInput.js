65ebdecf9193f352d1d27ea2a771d5c141b6e797
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-05-24 16:56:11 +0100

    Add username input logic

--- client/src/components/TextInput.js
+++ client/src/components/TextInput.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import styles from './TextInput.module.css';
 
-const TextInput = ({ styleName }) => <input className={styles[styleName]} type="text" />;
+const TextInput = ({ styleName, handleInputChange, username }) => (
+  <input className={styles[styleName]} type="text" name="username" onChange={handleInputChange} value={username} />
+);
 
 export default TextInput;
--- client/src/containers/UserList.js
+++ client/src/containers/UserList.js
@@ -11,18 +11,30 @@ export default class UserList extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      username: '',
+      onlineUsers: [],
+    };
+
     this.renderThumb = ({ style, ...props }) => {
       const thumbStyle = {
         backgroundColor: 'rgba(144, 164, 173, 0.5)',
       };
       return <div style={{ ...style, ...thumbStyle }} {...props} />;
     };
+
+    this.handleInputChange = event => {
+      const { name, value } = event.target;
+      this.setState({
+        [name]: value,
+      });
+    };
   }
   render() {
     return (
       <Fragment>
         <div className={styles.loginContainer}>
-          <TextInput styleName="loginInput" />
+          <TextInput styleName="loginInput" handleInputChange={this.handleInputChange} username={this.state.username} />
           <JoinButton size="small">Join</JoinButton>
         </div>
         <div className={styles.listContainer}>
